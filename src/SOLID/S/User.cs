b285d11306d41ecccc97e26270e0109c2ae500f9
CiReal <Kydan301193@gmail.com>
2025-02-20 23:08:34 +0700

    C4-Solid-S) clear

---
+++ src/SOLID/S/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.src.SOLID.S
+{
+    public class User
+    {
+        public string Username {  get; set; }
+        public string Email {  get; set; }
+    }
+}
---
+++ src/SOLID/S/UserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.src.SOLID.S
+{
+    public class UserService
+    {
+        public void Register(User user)
+        {
+            //Register user logic...
+
+            //Send email
+            EmailSender emailSender = new EmailSender();
+            emailSender.SendEmail(user.Email, "Welcome to ours platform");
+        }
+    }
+}
