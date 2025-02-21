16e8488f588ea03e158aa3b581c2227f7cdaf6dd
CiReal <Kydan301193@gmail.com>
2025-02-21 09:53:05 +0700

    C5-SOLID-O

---
+++ src/SOLID/O/Shape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.src.SOLID.O
+{
+    public abstract class Shape
+    {
+        public abstract double CalculateArea();
+    }
+}
