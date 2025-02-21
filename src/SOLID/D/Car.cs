6df9206f23b7366ae919fdf50031ee4c3d10f74a
CiReal <Kydan301193@gmail.com>
2025-02-21 10:48:56 +0700

    C8-SOLID-D

---
+++ src/SOLID/D/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.src.SOLID.D
+{
+    public class Car
+    {
+        private IEngine engine;
+        public Car(IEngine engine)
+        {
+            this.engine = engine;
+        }
+
+        public void StartCar()
+        {
+            engine.Start();
+            System.Console.WriteLine("Car started.");
+        }
+    }
+}
+/*
+ *  Bây giờ có thể đưa bất kỳ loại engine nào vào car
+    var engine = new Engine(); // concrete implementation to be "injected" into the car
+
+    var car = new Car(engine);
+
+    car.StartCar();var engine = new Engine(); // concrete implementation to be "injected" into the car
+*/
+
---
+++ src/SOLID/D/Engine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.src.SOLID.D
+{
+    public class Engine : IEngine
+    {
+        public void Start()
+        {
+            System.Console.WriteLine("Engine started.");
+        }
+    }
+}
+//Các mô-đun cấp cao không nên phụ thuộc vào các mô-đun cấp thấp. Cả hai đều nên phụ thuộc vào sự trừu tượng.
