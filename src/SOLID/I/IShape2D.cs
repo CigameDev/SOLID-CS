6ad3239b631077f352dc19553aefd06f155dc3fa
CiReal <Kydan301193@gmail.com>
2025-02-21 10:28:13 +0700

    C7-SOLID-I

---
+++ src/SOLID/I/IShape2D.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.src.SOLID.I
+{
+    public interface IShape2D
+    {
+        double Area();
+    }
+}
+/*
+ * Chia nhỏ các interface ra 
+ * Ví dụ IShape có 2 phương thức là diện tích và thể tích thì hình 2D cko có thể tích 
+ * Tách thành IShape2D và IShape3D
+ */
---
+++ src/SOLID/I/Sphere.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.src.SOLID.I
+{
+    public class Sphere : IShape3D
+    {
+        public double Radius { get; set; }
+        public double Area()
+        {
+            return (4.0/3.0)*Math.PI *Math.Pow(Radius,3);
+        }
+
+        public double Volume()
+        {
+            return 4*Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+}
