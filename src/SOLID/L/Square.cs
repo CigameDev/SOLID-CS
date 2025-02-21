46eb00b7c574e8b5b491396ceb104d1589e3594c
CiReal <Kydan301193@gmail.com>
2025-02-21 10:17:31 +0700

    C6-SOLID-L

---
+++ src/SOLID/L/Square.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.src.SOLID.L
+{
+    public class Square : Shape
+    {
+        private double sideLength;
+        public double SideLength
+        {
+            get => sideLength;
+            set => sideLength = value;
+        }
+
+        public override double Area => sideLength *sideLength;
+    }
+    /*
+        Shape rectangle = new Rectangle { Width = 5, Height = 4 };
+
+        Console.WriteLine($"Area of the rectangle: {rectangle.Area}");
+
+        Shape square = new Square { SideLength = 5 };
+
+        Console.WriteLine($"Area of the square: {square.Area}");
+     */
+
+   
+
+}
